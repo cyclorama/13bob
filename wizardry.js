ad6335ff9c20543d0cdaa97b18d5021056c1835c
bloodbag <bloodbag@users.noreply.github.com>
2018-05-04 14:10:56 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -13,15 +13,12 @@
            -....--'  / '  `'-'`               '  `'-'`   
            `.. __..-'                                    
 */
-var hornForward =			new Audio('vibrations/flower_forward.mp3');hornForward.volume=0.15;
-var hornReverse=			new Audio('vibrations/flower_reverse.mp3');hornReverse.volume=0.15;
-var stretchForward =		new Audio('vibrations/stretch_forward.mp3');stretchForward.volume=0.15;
-var stretchReverse =		new Audio('vibrations/stretch_reverse.mp3');stretchReverse.volume=0.15;
-var space =					new Audio('vibrations/space.mp3');space.volume=0.15;
-var projector =				new Audio('vibrations/projector.mp3');projector.volume=0.15;
-var chatter =				new Audio('vibrations/chatter.mp3');chatter.volume=0.15;
-var bombForward =			new Audio('vibrations/inflate.mp3');bombForward.volume=0.15;
-var bombReverse =			new Audio('vibrations/deflate.mp3');bombReverse.volume=0.15;
+var sounds = 			[ 	[ new Audio('vibrations/flower_forward.mp3'), new Audio('vibrations/flower_reverse.mp3') ],
+			 				[ new Audio('vibrations/stretch_forward.mp3'), new Audio('vibrations/stretch_reverse.mp3') ],
+			 				[ new Audio('vibrations/space.mp3') ],
+			 				[ new Audio('vibrations/projector.mp3') ],
+			 				[ new Audio('vibrations/chatter.mp3') ],
+			 				[ new Audio('vibrations/inflate.mp3'), new Audio('vibrations/deflate.mp3')] ]
 var mixtape =				new Audio('mixtapes/E R R S T H E T I C/01 Strawberry Switchblade - Who Knows What Love Is.mp3');
 var tracks =			[	"01 Strawberry Switchblade - Who Knows What Love Is.mp3",
 							"02 Pebbles - Why Do I Believe.mp3",
@@ -62,6 +59,7 @@ function highlightTrack(track) {
 
 function loadGifs() {
 	if (typeof window.orientation !== 'undefined') {
+		for (var i = 0; i < )
 		document.flower.src='photos/framed_flower_forward.gif';document.flower.src='photos/framed_flower_reverse.gif';document.flower.src='photos/framed_flower.gif';
 		document.eye.src='photos/framed_eye_forward.gif';document.eye.src='photos/framed_eye_reverse.gif';document.eye.src='photos/framed_eye.gif';
 		document.golden.src='photos/framed_spiral_forward.gif';document.golden.src='photos/framed_spiral.gif';
@@ -74,17 +72,17 @@ function loadGifs() {
 
 function showMenu(option)	{ document.getElementById("menu").innerHTML="";menu=!menu;for(var i=0;i<items[option].length;i++){document.getElementById("menu").innerHTML+=menu?"<a style='padding:13em;' href='"+items[option][i]+"'><img id='menu' src='"+items[option][i]+"/logo.png'/></a>":"";} }
 function fibonacci(nterms)	{ var n1=0;var n2=1;var next;var goldenRatio;for(var i=0;i<nterms;i++){next=n1+n2;n1=n2;n2=next;goldenRatio=n2/n1;console.log(next+" (Golden ratio = "+goldenRatio+")");} }
-function playHorn()			{ hornReverse.pause();hornReverse.currentTime=0;hornForward.play(); }
-function stopHorn()			{ hornForward.pause();hornForward.currentTime=0; hornReverse.play(); }
-function playStretch()		{ stretchReverse.pause();stretchReverse.currentTime=0;stretchForward.play(); }
-function stopStretch()		{ stretchForward.pause();stretchForward.currentTime=0;stretchReverse.play(); }
-function playSpace()		{ space.play(); }
-function stopSpace()		{ space.pause();space.currentTime=0; }
-function playProjector()	{ projector.play(); }
-function stopProjector()	{ projector.pause();projector.currentTime=0; }
-function playChatter()		{ chatter.play(); }
-function stopChatter()		{ chatter.pause();chatter.currentTime=0; }
-function playBomb()			{ bombReverse.pause();bombReverse.currentTime=0;bombForward.play(); }
-function stopBomb()			{ bombForward.pause();bombForward.currentTime=0;bombReverse.play(); }
+function playHorn()			{ sounds[0][1].pause();sounds[0][1].currentTime=0;sounds[0][0].play(); }
+function stopHorn()			{ sounds[0][0].pause();sounds[0][0].currentTime=0; sounds[0][1].play(); }
+function playStretch()		{ sounds[1][1].pause();sounds[1][1].currentTime=0;sounds[1][0].play(); }
+function stopStretch()		{ sounds[1][0].pause();sounds[1][0].currentTime=0;sounds[1][1].play(); }
+function playSpace()		{ sounds[2][0].play(); }
+function stopSpace()		{ sounds[2][0].pause();sounds[2][0].currentTime=0; }
+function playProjector()	{ sounds[3][0].play(); }
+function stopProjector()	{ sounds[3][0].pause();sounds[3][0].currentTime=0; }
+function playChatter()		{ sounds[4][0].play(); }
+function stopChatter()		{ sounds[4][0].pause();sounds[4][0].currentTime=0; }
+function playBomb()			{ sounds[5][1].pause();sounds[5][1].currentTime=0;sounds[5][0].play(); }
+function stopBomb()			{ sounds[5][0].pause();sounds[5][0].currentTime=0;sounds[5][1].play(); }
 
 playTape();
