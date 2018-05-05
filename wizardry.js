022a017f331c7f4ed3d0245bb0ec49cd04398e89
bloodbag <bloodbag@users.noreply.github.com>
2018-05-05 07:45:07 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -33,8 +33,7 @@ title = 					"E R R S T H E T I C  /  V O L U M E  O N E\n"+
 							"[05 The Jets - The Only Dance]\n",
 tapePlay =					false,
 track =						0,
-menu = 						false;
-items = [ ["0", "0"], ["1", "1"], ["games/fisher", "games/slingshoot"], ["3", "3"], ["4", "4"], ["5", "5"] ];
+items =						[ ["0", "0"], ["1", "1"], ["games/fisher", "games/slingshoot"], ["3", "3"], ["4", "4"], ["5", "5"] ];
 for (var x = 0; x < sounds.length; x++) for (var y = 0; y < sounds[x].length; y++) sounds[x][y].volume = 0.15;
 
 function tape() { tapePlay = !tapePlay;
