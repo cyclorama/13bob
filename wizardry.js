931dce2cc2e92bee07ec30f67fb96b736775edca
bloodbag <bloodbag@users.noreply.github.com>
2018-05-26 01:42:55 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -42,7 +42,7 @@ function pTape() { tapeP = !tapeP;
 		document.tape.src = 'photos/framed_tape.gif';
 		track.pause(); track.currentTime = 0;
 		trackN = trackN < tracks.length - 1 ? trackN + 1 : 0;
-		track = new Audio(track.src.replace(tracks[trackN - 1], ""));
+		track = new Audio(track.src.replace(tracks[0], tracks[1]));
 	}
 }
 
