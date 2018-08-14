8a8ab0c09b3328a8b0921115b9e2246bf3865687
bloodbag <bloodbag@users.noreply.github.com>
2018-08-14 10:05:33 +0100

    Update beautify.js

--- steam/beautify.js
+++ steam/beautify.js
@@ -19,8 +19,6 @@ function beautify() {
 	for (var i = 1; i < c.length; i++) {
 		msg = l(c[i]); ul = l(c[i - 1]); usr = ul[ul.length - 2];
 		lc = i < c.length - 1 ? msg.length - 2 : msg.length;
-		for (var j = 0; j < lc; j++) {
-			document.getElementById('chat').value += usr + ' : ' + msg[j] + '\n';
-		}
+		[...Array(lc)].forEach((_, j) => ($scope.chat += usr + ' : ' + msg[j] + '\n');
 	}
 }
