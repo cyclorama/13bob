36870061738b820c7ef75a0b9aa09cfce4eb50d3
bloodbag <bloodbag@users.noreply.github.com>
2018-10-18 23:12:09 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -11,7 +11,7 @@ tracks =		[	'01 Strawberry Switchblade - Who Knows What Love Is.mp3',
 					'04 Dionne Warwick - Can\'t Hide Love.mp3',
 					'05 The Jets - The Only Dance.mp3'	],
 track =				new Audio(`mixtapes/E R R S T H E T I C/${tracks[0]}`),
-items =				[ ['+'], ['', ''], ['fisher', 'slingshoot', 'ptolemy'], ['', ''], ['http://steamcommunity.com/id/huh'], ['-'] ],
+items =				[ ['+'], [''], ['fisher', 'slingshoot', 'ptolemy'], [''], ['http://steamcommunity.com/id/huh'], ['-'] ],
 images =			new Array(),
 tapeP =				false,
 trackN =			0;
