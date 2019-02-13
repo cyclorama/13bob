593a53b998dae31ecb02f690dfd2cb1c10a7b67a
bloodbag <bloodbag@users.noreply.github.com>
2019-02-13 10:32:26 +0000

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -18,7 +18,7 @@ menu =				false,
 tapeP =				false,
 trackN =			0;
 
-sounds.forEach(sound => sound.forEach(property => property.volume = 0.05));
+sounds.forEach(sound => sound.forEach(property => property.volume = 0.025));
 tracks.forEach(song => title += `[${song.replace('.mp3', '')}]\n`);
 
 const images = [...nav].map(p => {
