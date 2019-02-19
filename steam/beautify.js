8f6fec0e7e3d15458d8b712a201bd2e9d0f991eb
bloodbag <bloodbag@users.noreply.github.com>
2019-02-19 15:38:02 +0000

    Delete beautify.js

--- steam/beautify.js
+++
@@ -1,8 +0,0 @@
-function beautify() {
-	var ul, msg, usr, i = 1, j, a = () => document.getElementById('chat'), l = (m) => m.split('\n'), c = a().value.split(':\n'); a().value = '';
-	for (; i < c.length; i++) {
-		msg = l(c[i]); ul = l(c[i - 1]); usr = ul[ul.length - 2];
-		lc = i < c.length - 1 ? msg.length - 2 : msg.length;
-		for (j = 0; j < lc;) a().value += `${usr} : ${msg[j++]}\n`;
-	}
-}
