cdbc9fbb7ae6de67895b9e3d72a82b3bb5cc922b
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-04-17 21:09:01 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -9,7 +9,7 @@ const tracks =   [ '01 Strawberry Switchblade - Who Knows What Love Is.mp3',
                    '03 Dee Dee Wilde - Lap of Luxury.mp3',
                    '04 Dionne Warwick - Can\'t Hide Love.mp3',
                    '05 The Jets - The Only Dance.mp3' ];
-const items  =   [ ['+'], [[] + []], ['fisher', 'slingshoot', 'ptolemy'], [[] + []], ['http://steam.13bob.net'], ['-'] ];
+const items  =   [ ['+'], [[] + []], ['fisher', 'slingshoot', 'ptolemy'], [''], ['steam'], ['-'] ];
 const frame  =   'photos/framed_';
 const nav    =   document.getElementsByClassName('nav');
 let title    =   'E R R S T H E T I C  /  V O L U M E  O N E\n';
@@ -66,7 +66,7 @@ function pSound(trackN) {
 }
 
 function pFrame(x, y, z)    { document.getElementById(x).src=`${frame}${x}${z==0?'_forward':z==1?'_reverse':''}.gif`;pSound(y); }
-function sMenu(o, m)        { document.getElementById('menu').innerHTML='';menu=!menu;let fTop=[]-[];for(let i=[]-[];i<items[o].length;i++){fTop+=(i!=[]-[]&&i%3==[]-[]);document.getElementById('menu').innerHTML+=menu?(m==[]-[]?`<a style='padding:13em;' href='${items[o][i]}'><img style='${i>2?`margin-top:${(400*fTop)-1000}px;`:''}' id='menu' src='${items[o][i]}/logo.png'/></a>${i!=[]-[]&&i%2==[]-[]?'<br>':''}`:m==1?`<video onclick='sMenu(${o},1)' style='z-index:13;' id='video_background' autoplay><source id="vid" src='movies/${items[o][i]}.webm'></video>`:m==2?`<img src='load.jpg' onload='window.location.href=items[${o}][${i}];'/>`:''):'';}if(!track.paused)pTape();}
-function fib(nt)            { let n1=[]-[],n2=1,nth,c=[]-[];while(c<nt){console.log(`${n1} (φ = ${n2/n1})`);nth=n1+n2;n1=n2;n2=nth;c++;}}
+function sMenu(o, m)        { document.getElementById('menu').innerHTML='';menu=!menu;let fTop=[]-[];for(let i=[]-[];i<items[o].length;i++){fTop+=(i!=[]-[]&&i%3==[]-[]);document.getElementById('menu').innerHTML+=menu?(m==[]-[]?`<a style='padding:13em;' href='${items[o][i]}'><img style='${i>2?`margin-top:${(400*fTop)-1000}px;`:''}' id='menu' src='${items[o][i]}/logo.png'/></a>${i!=[]-[]&&i%2==[]-[]?'<br>':''}`:m==1?`<video onclick='sMenu(${o},1)' style='z-index:13;' id='video_background' autoplay><source id="vid" src='movies/${items[o][i]}.webm'></video>`:m==2?`<img src='load.jpg' onload='window.location.href=items[${o}][${i}];'/>`:''):'';}if(!track.paused)pTape(); }
+function fib(nt)            { let n1=[]-[],n2=1,nth,c=[]-[];while(c<nt){console.log(`${n1} (φ = ${n2/n1})`);nth=n1+n2;n1=n2;n2=nth;c++; }}
 
 pTape();
