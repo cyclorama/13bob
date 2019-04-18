f0affb80e335e0fd6321832af60cf6affed9e454
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-04-18 16:46:07 +0100

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,4 +1,5 @@
-document.querySelector('body').addEventListener('mousemove', (e) => {
+const body = document.querySelector('body');
+body.addEventListener('mousemove', (e) => {
 	body.style.backgroundPositionX = -e.offsetX + 'px';
 	body.style.backgroundPositionY = -e.offsetY + 'px';
 });
