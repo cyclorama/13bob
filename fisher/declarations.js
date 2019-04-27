506324d5725919be2dd02247bc99f44a4748f895
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-04-27 22:01:18 +0100

    Update declarations.js

--- fisher/declarations.js
+++ fisher/declarations.js
@@ -177,15 +177,15 @@ class Hook {
 	checkCollision() {
 		for (let i = 0; i < rocks.length; i++) {
 			if (this.scaleX == 1) {
-				if (this._x == this.x &&
-					this._y > this.y - this._scaleY - 1 &&
-					this._y < this.y + this._scaleY + 1) {
+				if (this._x == this._x &&
+					this._y > this._y - this._scaleY - 1 &&
+					this._y < this._y + this._scaleY + 1) {
 					this.reelHook();
 				}
-			} else if (	this._x > this.x - (this._scaleX + 1) &&
-						this._x < this.x + (this._scaleX + 1) &&
-						this._y > this.y - (this._scaleY + 1) &&
-						this._y < this.y + (this._scaleY + 1)) {
+			} else if (	this._x > this._x - (this._scaleX + 1) &&
+						this._x < this._x + (this._scaleX + 1) &&
+						this._y > this._y - (this._scaleY + 1) &&
+						this._y < this._y + (this._scaleY + 1)) {
 				this.reelHook();
 			}
 		}
