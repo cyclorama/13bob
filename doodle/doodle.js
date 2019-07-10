e47255ed7d1ca3c0e597595ca786dc654a66f432
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-10 03:48:07 +0100

    Update doodle.js

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -121,26 +121,6 @@ window.onload = () => {
 		context.closePath();
 		context.stroke();
 	}
-
-	function getMousePos(canvas, evt) {
-		let rect = canvas.getBoundingClientRect();
-		return {
-			x: evt.clientX - rect.left,
-			y: evt.clientY - rect.top
-		};
-	}
-
-	function getDistance(x1, y1, x2, y2) {
-		return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-	}
-
-	function getCoordFromAngle(angle, distance) {
-		angle *= Math.PI / 180;
-		return {
-			x: distance * Math.cos(angle),
-			y: distance * Math.sin(angle)
-		};
-	}
-
+	
 	init();
 }
