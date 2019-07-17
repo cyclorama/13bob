6d3aa6b012a904cf1b2926d37f3e0108b1cd3761
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-17 12:08:26 +0100

    Update ptolemy.js

--- ptolemy/ptolemy.js
+++ ptolemy/ptolemy.js
@@ -110,7 +110,7 @@ window.onload = () => {
 	}
 
 	function getDistance(x1, y1, x2, y2) {
-		return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 	}
 
 	function getCoordFromAngle(angle, distance) {
