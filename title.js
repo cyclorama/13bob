df6f6f98f36e7d68f805f0d0efdf9120838f1b64
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-20 00:29:27 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,6 +1,6 @@
 (async () => {
     const h1 = document.querySelector('h1'), sleep = time => new Promise(resolve => setTimeout(resolve, time));
-    ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].forEach(colour => {
+    ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].forEach(async colour => {
         h1.innerHTML = `<a href="//13bob.net/"><font color=\'${colour}\'>13bob</font></a>`;
         await sleep(750);
     });
