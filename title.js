232c9409ab87263d62d1127b4b552a747a9fc3f7
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-20 00:27:37 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,13 +1,7 @@
-
-    const h1 = document.querySelector('h1');
-    
-    function sleep(time) { return new Promise(resolve => setTimeout(resolve, time)) };
-
-    async function asd() {
-        ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].forEach(colour => {
-            h1.innerHTML = `<a href="//13bob.net/"><font color=\'${colour}\'>13bob</font></a>`;
-            await sleep(750);
-        });
-    }
-
-    asd();
+(async () => {
+    const h1 = document.querySelector('h1'), sleep = time => new Promise(resolve => setTimeout(resolve, time));
+    ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].forEach(colour => {
+        h1.innerHTML = `<a href="//13bob.net/"><font color=\'${colour}\'>13bob</font></a>`;
+        await sleep(750);
+    });
+});
