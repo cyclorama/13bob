42497cbee7ec3292c083db0da21e3fb0bf78162c
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-20 01:28:24 +0100

    Update title.js

--- title.js
+++ title.js
@@ -3,6 +3,7 @@
     colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
     
     h1.style.animation = 'none';
+    await sleep(1000);
     
     for (const colour of colours) {
         h1.innerHTML = `<a href="//13bob.net/"><font color=\'${colour}\'>13bob</font></a>`;
