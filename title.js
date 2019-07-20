b37a04fea43bc0cb1ad07dd920687988600765de
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-20 01:25:28 +0100

    Update title.js

--- title.js
+++ title.js
@@ -2,7 +2,7 @@
     const h1 = document.querySelector('h1'), sleep = time => new Promise(resolve => setTimeout(resolve, time)),
     colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
     
-    h1.style = 'animation:none;';
+    h1.style.animation = 'none';
     
     for (const colour of colours) {
         h1.innerHTML = `<a href="//13bob.net/"><font color=\'${colour}\'>13bob</font></a>`;
