25191bf02dba1ae70c16dcc2270b94602d59acc5
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-20 04:04:56 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,14 +1,13 @@
 (async () => {
     const h1 = document.querySelector('h1'), sleep = time => new Promise(resolve => setTimeout(resolve, time)),
     colours  = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], title = '13bob';
-    let c = 0;
+    let c = 0, html;
 
     await sleep(1000);
     h1.style.animation = 'none';
 
-    let html = '';
-
     while (c < 12) {
+        html = '';
         for (let i = 0; i < colours.length; i++) {
 
             if (c - i > title.length) {
