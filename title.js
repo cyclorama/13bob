82f9663503cc67f4ccc1ab214c7658ffc2c15282
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-25 23:00:12 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,6 +1,6 @@
-(async () => { // Genesis 9:13 — I do set my bow in the cloud, and it shall be for a token of a covenant between me and the earth.
+(async (title) => { // Genesis 9:13 — I do set my bow in the cloud, and it shall be for a token of a covenant between me and the earth.
     const h1 = document.querySelector('h1'), sleep = time => new Promise(resolve => setTimeout(resolve, time)),
-    colours  = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], title = '13bob';
+    colours  = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
     let position = 0, html;
 
     await sleep(1000);
@@ -20,4 +20,4 @@
         await sleep(35);
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
-})();
+})('13bob');
