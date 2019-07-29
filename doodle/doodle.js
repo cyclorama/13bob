8d25a2152c4c1f888a832b14f2057b4b6aed1040
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-07-29 04:56:17 +0100

    Update doodle.js

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -70,14 +70,14 @@ window.onload = () => {
 		let numOfFilters = getRand(numOfShapes);
 
 		context.globalCompositeOperation = ['source-over', 'source-in', 'source-out',
-											'source-atop', 'destination-over', 'destination-in',
-											'destination-out', 'destination-atop', 'lighter',
-											'copy', 'xor', 'multiply',
-											'screen', 'overlay', 'darken',
-											'lighten', 'color-dodge', 'color-burn',
-											'hard-light', 'soft-light', 'difference',
-											'exclusion', 'hue', 'saturation',
-											'color', 'luminosity'][getRand(26)];
+                                            'source-atop', 'destination-over', 'destination-in',
+                                            'destination-out', 'destination-atop', 'lighter',
+                                            'copy', 'xor', 'multiply',
+                                            'screen', 'overlay', 'darken',
+                                            'lighten', 'color-dodge', 'color-burn',
+                                            'hard-light', 'soft-light', 'difference',
+                                            'exclusion', 'hue', 'saturation',
+                                            'color', 'luminosity'][getRand(26)];
 
 		for (let i = 0; i < numOfFilters; i++) {
 			switch (getRand(5)) {
