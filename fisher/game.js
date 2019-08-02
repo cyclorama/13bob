9aec6bf614193bcc03aa2ce45d2d3147b43a94fe
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-02 02:10:47 +0100

    Update game.js

--- fisher/game.js
+++ fisher/game.js
@@ -1,33 +1,33 @@
 function update() {
-	hook.update();
+    hook.update();
 
-	for (let i = 0; i < fishes.length; i++) {
-		fishes[i].update();
-	}
+    for (let i = 0; i < fishes.length; i++) {
+        fishes[i].update();
+    }
 }
 
 function render() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-	hook.render();
-	renderLine();
+    hook.render();
+    renderLine();
 
-	for (let i = 0; i < fishes.length; i++) {
-		fishes[i].render();
-	}
+    for (let i = 0; i < fishes.length; i++) {
+        fishes[i].render();
+    }
 
-	for (let i = 0; i < rocks.length; i++) {
-		rocks[i].render();
-	}
-	document.getElementById('boat').src = `img/boat${PLAYER_CAUGHT}.png`;
+    for (let i = 0; i < rocks.length; i++) {
+        rocks[i].render();
+    }
+    document.getElementById('boat').src = `img/boat${PLAYER_CAUGHT}.png`;
 }
 
 async function main() {
-	update();
-	render();
+    update();
+    render();
 
-	await sleep(sleepTime);
-	requestAnimationFrame(main);
+    await sleep(sleepTime);
+    requestAnimationFrame(main);
 }
 
 document.getElementById('score').innerText = PLAYER_SCORE;
