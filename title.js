8376f28dfdbe2b5f17b8da3a4246a39d2a50cf12
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-14 05:05:01 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,14 +1,14 @@
 (async (title, position, html, colours, sleep) => { // Genesis 9:13 — I do set my bow in the cloud, and it shall be for a token of a covenant between me and the earth.
     const h1 = document.querySelector('h1');
 
-    await sleep(1000).then(h1.style.animation = 'none');
+    await sleep(1000).then(() => h1.style.animation = 'none');
 
     while (position < 12) { html = '';
         for (let i = 0; i < colours.length; i++) {
             if (position - i > title.length) continue;
             if (title[position - i] != null) html = `<font color="${colours[i]}">${title[position - i]}</font>${html}`;
         }
-        await sleep(35).then(h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`, position++);
+        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`, position++);
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
