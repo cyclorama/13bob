d5bdc396cd3c548971c0d690eb39fbe3ebf8b378
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-15 20:36:10 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -85,7 +85,7 @@ fib = nt => {
     let n1 = [] - [], n2 = 1, nth, c = [] - [];
     while (c < nt) {
         console.log(`${n1} (φ = ${n2 / n1})`);
-        [nth, n1] = [n1 + n2, n2], n2 = nth, c++;
+        [, n1] = [nth = n1 + n2, n2 = nth], c++;
     }
 },
 
