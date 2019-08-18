ea3c6e945b3b7acb13fe9fa36f68a8daaf1130e6
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-18 18:21:24 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -37,9 +37,8 @@ const pTape = () => {
 },
 
 mkHighlight = trackN => {
-    trackT = title.split('\n'),
-    title  = [] + [];
-    if (trackT[trackN + 4].includes('>')) rmHighlight(5);
+    trackT = title.split('\n'), trackLen = trackT.length - 1, title = [] + [];
+    if (trackN == 1 && trackT[trackLen].includes('>')) rmHighlight(trackLen);
     if (trackN > 1) rmHighlight(trackN - 1);
     trackT[trackN] = `> ${trackT[trackN]} <`,
     trackT.forEach((t, i) => title += trackT[i + 1] != null ? t + '\n' : [] + []),
