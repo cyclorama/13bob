bc00c058fe0c45e10445307e7c77862fe3da82c3
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-19 17:55:06 +0100

    Update ptolemy.js

--- ptolemy/ptolemy.js
+++ ptolemy/ptolemy.js
@@ -59,7 +59,7 @@ window.onload = () => {
                 drawText('+', ((centerX / 2) + 75)  - (mouseDist / 2), centerY);
                 drawText('=', ((centerX / 2) + 175) - (mouseDist / 2), centerY);
                 drawRectangle(((centerX / 2) + 200) - (mouseDist / 2), centerY - (cond[0] / 2), 50, cond[0], colours[i]);
-                drawRectangle(((centerX / 2) + 0)   - (mouseDist / 2), centerY - (cond[base[0] - base[1]] / 2), 50, cond[base[0] - base[1]], colours[base[0]]);
+                drawRectangle(((centerX / 2) + 0)   - (mouseDist / 2), centerY - (cond[base[1] - base[0]] / 2), 50, cond[base[1] - base[0]], colours[base[0]]);
                 drawRectangle(((centerX / 2) + 100) - (mouseDist / 2), centerY - (cond[base[1]] / 2), 50, cond[base[1]], colours[base[1]]);
             }
         });
