b7a9dd68f3ae48a59ef103b86e128954e0e60a6b
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-08-20 20:17:06 +0100

    Update ptolemy.js

--- ptolemy/ptolemy.js
+++ ptolemy/ptolemy.js
@@ -56,11 +56,11 @@ window.onload = () => {
 
         [[pDist, qDist, rDist], [qDist, pDist, rDist], [rDist, qDist, pDist]].forEach((cond, i) => {
             if (cond[0] > cond[1] && cond[0] > cond[2]) { const base = [i != 0 ? 2 : 1, i != 0 ? 1 : 2, (i + 1) % 3, (i + 2) % 3];
-                drawText('+', ((centerX) + 50)  - (mouseDist / 2), 400 + centerY);
-                drawText('=', ((centerX) + 175) - (mouseDist / 2), 400 + centerY);
-                drawRectangle(((centerX) + 150) - (mouseDist / 2), 400 + centerY - (cond[0] / 4), 25, cond[0], colours[i]);
-                drawRectangle(((centerX) + 0)   - (mouseDist / 2), 400 + centerY - (cond[base[0]] / 4), 25, cond[base[0]], colours[base[2]]);
-                drawRectangle(((centerX) + 50) - (mouseDist / 2), 400 + centerY - (cond[base[1]] / 4), 25, cond[base[1]], colours[base[3]]);
+                drawText('+', ((centerX) + 50)  - (mouseDist / 4), 400 + centerY);
+                drawText('=', ((centerX) + 165) - (mouseDist / 4), 400 + centerY);
+                drawRectangle(((centerX) + 150) - (mouseDist / 4), 400 + centerY - (cond[0] / 4), 25, cond[0], colours[i]);
+                drawRectangle(((centerX) + 0)   - (mouseDist / 4), 400 + centerY - (cond[base[0]] / 4), 25, cond[base[0]], colours[base[2]]);
+                drawRectangle(((centerX) + 50) - (mouseDist / 4), 400 + centerY - (cond[base[1]] / 4), 25, cond[base[1]], colours[base[3]]);
             }
         });
 
@@ -76,7 +76,7 @@ window.onload = () => {
     }
 
     function drawText(txt, x, y) {
-        context.font      = '50px Verdana';
+        context.font      = '25px Verdana';
         context.textAlign = 'center';
         context.fillStyle = 'white';
         context.fillText(txt, x, y);
