add807677bb92cd519baa90e87327d3cb0ca6647
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-09-10 23:39:57 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,14 +1,15 @@
 (async (title, position, html, colours, sleep) => { // Genesis 9:13 — I do set my bow in the cloud, and it shall be for a token of a covenant between me and the earth.
-    $(this).ready(async () => {
-        await sleep(1000).then(() => $("h1").css('animation', 'none'));
+    const h1 = document.querySelector('h1');
 
-        while (position < 12) { html = '';
-            for (let i = 0; i < colours.length; i++) {
-                if (position - i > title.length) continue;
-                if (title[position - i] != null) html = `<font color="${colours[i]}">${title[position - i]}</font>${html}`;
-            }
-            await sleep(35).then(() => $("h1").html(`<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`, position++));
+    await sleep(1000).then(() => h1.style.animation = 'none');
+
+    while (position < 12) { html = '';
+        for (let i = 0; i < colours.length; i++) {
+            if (position - i > title.length) continue;
+            if (title[position - i] != null) html = `<font color="${colours[i]}">${title[position - i]}</font>${html}`;
         }
-        $("h1").css('animation', 'heartbeat 1s alternate infinite');
-      });
+        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`, position++);
+    }
+    h1.style.animation = 'heartbeat 1s alternate infinite';
+
 })('13bob', [] - [], [] + [], ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], time => new Promise(resolve => setTimeout(resolve, time)));
