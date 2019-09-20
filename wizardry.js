51d3188b65f9a7aa36ae36ec014034dcf21bf777
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-09-20 16:07:10 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -9,8 +9,8 @@ const sounds = [ [ new Audio('vibrations/horn_forward.ogg'   ), new Audio('vibra
                    '03 Dee Dee Wilde - Lap of Luxury.mp3',
                    '04 Dionne Warwick - Can\'t Hide Love.mp3',
                    '05 The Jets - The Only Dance.mp3' ],
-      items  = [ ['+'                 ], ['//13bob.net/words'  ], ['fisher', 'slingshoot', 'ptolemy'],
-                 ['//steam.13bob.net' ], ['//twitter.13bob.net'], ['-'                              ] ],
+      items  = [ ['+'                                        ], ['//13bob.net/words'  ], ['fisher', 'slingshoot', 'ptolemy'],
+                 ['//spotify.13bob.net', '//steam.13bob.net' ], ['//twitter.13bob.net'], ['-'                              ] ],
       frame  = 'photos/framed_',
       nav    = document.getElementsByClassName('nav'),
       dir    = 'mixtapes/E R R S T H E T I C/';
