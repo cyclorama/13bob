d98f5f3faab6e28fe6179e6eab88df4e5b5fe7d3
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-09-21 20:58:43 +0100

    Update ptolemy.js

--- ptolemy/ptolemy.js
+++ ptolemy/ptolemy.js
@@ -78,8 +78,8 @@ window.onload = () => {
             if (cond[0] > cond[1] && cond[0] > cond[2]) { const y = [i != 0 ? 2 : 1, i != 0 ? 1 : 2];
                 drawText('+          =', centerX +   -5, centerY);
                 drawRectangle(-80      + centerX +  125, centerY - (cond[0]        / 8), 50, cond[0]    / 4, colours[i]);
-                drawRectangle(-80      + centerX +  -25, centerY - (cond[base[0]]) / 8 , 50, cond[y[0]] / 4, colours[(i + 1) % 3]);
-                drawRectangle(-80      + centerX +   50, centerY - (cond[base[1]]) / 8 , 50, cond[y[1]] / 4, colours[(i + 2) % 3]);
+                drawRectangle(-80      + centerX +  -25, centerY - (cond[y[0]]) / 8 , 50, cond[y[0]] / 4, colours[(i + 1) % 3]);
+                drawRectangle(-80      + centerX +   50, centerY - (cond[y[1]]) / 8 , 50, cond[y[1]] / 4, colours[(i + 2) % 3]);
             }
         });
 
