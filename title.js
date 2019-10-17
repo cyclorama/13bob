84ac1b4c2ff887b8da7c5ad2956ad96d8c94dc52
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-10-17 05:49:00 +0100

    Update title.js

--- title.js
+++ title.js
@@ -3,13 +3,13 @@
 
     await sleep(1000).then(() => h1.style.animation = 'none');
 
-    while (position < 12) { html = []+[];
-        for (let i = +[]; i < colours.length; ) {
+    while (position < 12) { html = '';
+        for (let i = 0; i < colours.length; ) {
             if (position - i > title.length) continue;
             if (title[position - i]) html = `<font color="${colours[i]}">${title[position - i++]}</font>${html}`;
         }
-        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(+[], position - title.length - (!+[]+!+[]))}${html}${title.substring(position)}</a>`), position++;
+        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`), position++;
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
-}('13bob', +[], []+[], ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], time => new Promise(resolve => setTimeout(resolve, time)));
+}('13bob', 0, '', ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], time => new Promise(resolve => setTimeout(resolve, time)));
