76a0daf130d190b379a65490632b208f1b6dc815
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-10-17 05:57:39 +0100

    Update wizardry.js

--- wizardry.js
+++ wizardry.js
@@ -73,7 +73,7 @@ sMenu = (o, m) => { menu = !menu,
     for (let i = 0; i < items[o].length; i++) {
         fTop += (i != 0 && i % 3 == 0);
         document.getElementById('menu').innerHTML += menu ? (
-            m == 0 ? `<a style='padding: 13em;' href='${items[o][i]}'><img style='${i > 2 ? `margin-top: ${(400 * fTop) - 1000}px;` : []+[] }' id='menu' src='${items[o][i].replace('//', '')}/logo.png'/></a>${i != 0 && i % 2 == 0 ? '<br>' : '' }` :
+            m == 0 ? `<a style='padding: 13em;' href='${items[o][i]}'><img style='${i > 2 ? `margin-top: ${(400 * fTop) - 1000}px;` : '' }' id='menu' src='${items[o][i].replace('//', '')}/logo.png'/></a>${i != 0 && i % 2 == 0 ? '<br>' : '' }` :
             m == 1 ? `<video onclick='sMenu(${o}, 1)' style='z-index: 13;' id='video_background' autoplay><source id="vid" src='movies/${items[o][i]}.webm'></video>` :
             m == 2 ? `<img src='load.jpg' onload='window.location.href=items[${o}][${i}];'/>` : '') : '' ;
         }
