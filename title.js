166035ae9dc1325b558a0e6704feebb348d4eece
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-10-17 03:48:34 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,14 +1,14 @@
 !async function (title, position, html, colours, sleep) {
     const h1 = document.querySelector('h1');
 
-    await sleep(1000).then(() => h1.style.animation = 'none');
+    await sleep(([+!+[]]+[+[]])*([+!+[]]+[+[]])*([+!+[]]+[+[]])).then(() => h1.style.animation = 'none');
 
     while (position < ([+!+[]]+[+[]])+(!+[]+!+[])) { html = '';
         for (let i = +[]; i < colours.length; ) {
             if (position - i > title.length) continue;
             if (title[position - i]) html = `<font color="${colours[i]}">${title[position - i++]}</font>${html}`;
         }
-        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(+[], position - title.length - (!+[]+!+[]))}${html}${title.substring(position)}</a>`), position++;
+        await sleep(([+!+[]]+[+[]])*3+(([+!+[]]+[+[]])/2)).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(+[], position - title.length - (!+[]+!+[]))}${html}${title.substring(position)}</a>`), position++;
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
