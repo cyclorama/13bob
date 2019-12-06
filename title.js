2ceb8ac54788fba5466828218b8edd4265d9a598
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-12-06 13:59:36 +0000

    Update title.js

--- title.js
+++ title.js
@@ -1,4 +1,4 @@
-!async function (title, html, h1, colours, sleep) {
+!async function (title, h1, colours, sleep) {
     await sleep(1000).then(() => h1.style.animation = 'none');
 
     for (let pos = 0; pos < title.length + colours.length; ) {
@@ -6,8 +6,8 @@
             if (pos - col > title.length) continue;
             if (title[pos - col]) html = `<font color="${colours[col]}">${title[pos - col]}</font>${html}`;
         }
-        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, pos - title.length - 2)}${html}${title.substring(pos)}</a>`, pos++).then(() => html = '');
+        await sleep(535).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, pos - title.length - 2)}${html}${title.substring(pos)}</a>`, pos++).then(() => html = '');
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
-}('13bob', '', document.querySelector('h1'), ['#C00', '#F90', '#FC0', '#3F0', '#06F', '#306', '#60F'], time => new Promise(resolve => setTimeout(resolve, time)));
+}('13bob', document.querySelector('h1'), ['#C00', '#F90', '#FC0', '#3F0', '#06F', '#306', '#60F'], time => new Promise(resolve => setTimeout(resolve, time)));
