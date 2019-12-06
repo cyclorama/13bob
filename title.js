432b4fb76508a39bd081befb4b3c5341dfb9a07f
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-12-06 13:14:08 +0000

    Update title.js

--- title.js
+++ title.js
@@ -4,9 +4,9 @@
     await sleep(1000).then(() => h1.style.animation = 'none');
 
     for (let pos = 0; pos < title.length + colours.length; pos++) { html = '';
-        for (let i = 0; i < colours.length; i++) {
-            if (pos - i > title.length) continue;
-            if (title[position - i]) html = `<font color="${colours[i]}">${title[pos - i]}</font>${html}`;
+        for (let col = 0; col < colours.length; col++) {
+            if (pos - col > title.length) continue;
+            if (title[pos - col]) html = `<font color="${colours[col]}">${title[pos - col]}</font>${html}`;
         }
         await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, pos - title.length - 2)}${html}${title.substring(pos)}</a>`);
     }
