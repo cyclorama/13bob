583c379c39c7fc6117b00946bc056157c4dbf9c8
cyclorama <19229386+cyclorama@users.noreply.github.com>
2019-12-06 13:11:56 +0000

    Update title.js

--- title.js
+++ title.js
@@ -1,15 +1,15 @@
-!async function (title, position, html, colours, sleep) {
+!async function (title, html, colours, sleep) {
     const h1 = document.querySelector('h1');
 
     await sleep(1000).then(() => h1.style.animation = 'none');
 
-    while (position < title.length + colours.length) { html = '';
+    for (let pos = 0; pos < title.length + colours.length; pos++) { html = '';
         for (let i = 0; i < colours.length; i++) {
-            if (position - i > title.length) continue;
-            if (title[position - i]) html = `<font color="${colours[i]}">${title[position - i]}</font>${html}`;
+            if (pos - i > title.length) continue;
+            if (title[position - i]) html = `<font color="${colours[i]}">${title[pos - i]}</font>${html}`;
         }
-        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, position - title.length - 2)}${html}${title.substring(position)}</a>`, position++);
+        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substring(0, pos - title.length - 2)}${html}${title.substring(pos)}</a>`);
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
-}('13bob', 0, '', ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], time => new Promise(resolve => setTimeout(resolve, time)));
+}('13bob', '', ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'], time => new Promise(resolve => setTimeout(resolve, time)));
