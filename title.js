beb5a23954e42c6450b323d8ee480cd8d011985d
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-01-05 11:57:38 +0000

    Update title.js

--- title.js
+++ title.js
@@ -4,6 +4,7 @@
     for (let pos = 0; pos < title.length + colours.length; ) {
         for (let col = 0; col < colours.length; col++)
             if (title[pos - col] && pos - col < title.length) html = `<font color="${colours[col]}">${title[pos - col]}</font>${html}`;
+            
         await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substr(0, pos - title.length - 2)}${html}${title.substr(pos)}</a>`, pos++).then(() => html = '');
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
