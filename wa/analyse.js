3bf6d87a2324e9a28a664ed1e482ef1eab206907
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-24 23:33:01 +0000

    Update analyse.js

--- wa/analyse.js
+++ wa/analyse.js
@@ -1,3 +1,6 @@
 let textArea = document.getElementById("text");
-textArea.onchange = () => { textArea.value += textArea.value; };
-//textArea.oninput = () => { textArea.value += textArea.value; };
+//textArea.onchange = () => { textArea.value += textArea.value; };
+textArea.oninput = () => {
+    let wordCount = textArea.split().length;
+    console.log(wordCount);
+};
