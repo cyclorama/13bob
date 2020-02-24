e7aa28df3b8d873cb77f142f6b2542c7ce6abbb1
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-24 23:28:00 +0000

    Update analyse.js

--- wa/analyse.js
+++ wa/analyse.js
@@ -1,4 +1,3 @@
 let textArea = document.getElementById("text");
-console.log(textArea.value);
 //textArea.onchange = () => { textArea.value += textArea.value; };
-//textArea.oninput = () => { textArea.value += textArea.value; };
+textArea.oninput = () => { textArea.value += textArea.value; };
