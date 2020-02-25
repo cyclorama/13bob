f9fcdc478c83f567f34d0b5860b331a544635ef1
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-25 23:34:48 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -1,6 +1,9 @@
 let textArea = document.getElementById("text");
 let wcSpan = document.getElementById("wordCount");
-textArea.oninput = () => {
+
+textArea.oninput = () => onInput();
+
+let onInput = () => {
     let wordArr = textArea.value.split(" ").filter(el => { return el != "" });
     wcSpan.innerHTML = `${wordArr.length} word${wordArr.length != 1 ? "s" : ""}`;
 };
