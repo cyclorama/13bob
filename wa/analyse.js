71063f4015c4ce76dabc082c23e5c9441113421a
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-25 00:00:38 +0000

    Update analyse.js

--- wa/analyse.js
+++ wa/analyse.js
@@ -2,5 +2,5 @@ let textArea = document.getElementById("text");
 let wordCount = document.getElementById("wordCount");
 textArea.oninput = () => {
     let wordArr = textArea.value.split(" ").filter(el => { return el != "" });
-    wordCount.value = wordArr.length;
+    wordCount.innerHTML = wordArr.length;
 };
