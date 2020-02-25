25514f2c852ce061a2bb5627965afaf12c00f801
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-25 18:41:41 +0000

    Delete wa.js

--- wa/wa.js
+++
@@ -1,6 +0,0 @@
-let textArea = document.getElementById("text");
-let wcSpan = document.getElementById("wordCount");
-textArea.oninput = () => {
-    let wordArr = textArea.value.split(" ").filter(el => { return el != "" });
-    wcSpan.innerHTML = `${wordArr.length} word${wordArr.length != 1 ? "s" : ""}`;
-};
