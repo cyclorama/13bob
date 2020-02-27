698c98d8543179738bc103b6650e022acb283cf3
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-27 01:11:26 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -7,13 +7,13 @@ textArea.oninput = () => onInput();
 let onInput = () => {
     let totalWords = 0;
 
-    let wordArrWordCount = textArea.value.split("\n").filter(el => el != "").forEach(line => {
-            //totalWords += line.split(" ").filter(el => el != "").length;
+    let wordArrWords = textArea.value.split("\n").filter(el => el != "").array.forEach(line => {
+            totalWords += line.split(" ").filter(el => el != "").length;
             console.log(line);
         });
 
-    let wordArrNewline = textArea.value.split("\n").filter(el => el != "");
+    let wordArrLines = textArea.value.split("\n").filter(el => el != "");
 
     wcSpan.innerHTML = `${totalWords} word${totalWords != 1 ? "s" : ""}`;
-    lcSpan.innerHTML = `${wordArrNewline.length} line${wordArrNewline.length != 1 ? "s" : ""}`;
+    lcSpan.innerHTML = `${wordArrLines.length} line${wordArrLines.length != 1 ? "s" : ""}`;
 };
