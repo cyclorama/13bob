1c2afd3b2cf8d347f0ed943e45ad4ea6efac0c6e
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-27 00:39:33 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -1,9 +1,13 @@
 let textArea = document.getElementById("text");
 let wcSpan = document.getElementById("wordCount");
+let lcSpan = document.getElementById("lineCount");
 
 textArea.oninput = () => onInput();
 
 let onInput = () => {
-    let wordArr = textArea.value.split(" ").filter(el => { return el != "" });
-    wcSpan.innerHTML = `${wordArr.length} word${wordArr.length != 1 ? "s" : ""}`;
+    let wordArrSpace = textArea.value.split(" ").filter(el => { return el != "" });
+    let wordArrNewline = textArea.value.split("\n").filter(el => { return el != "" });
+
+    wcSpan.innerHTML = `${wordArrSpace.length} word${wordArrSpace.length != 1 ? "s" : ""}`;
+    lcSpan.innerHTML = `${wordArrNewline.length} line${wordArrNewline.length != 1 ? "s" : ""}`;
 };
