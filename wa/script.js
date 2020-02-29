0ef5f9cee7ea5fbdc580fa21c838393f27fa2705
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-02-29 00:15:13 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -1,14 +1,21 @@
 let textArea = document.getElementById("text"),
     lcSpan   = document.getElementById("lineCount"),
     wcSpan   = document.getElementById("wordCount"),
+    bSpan    = document.getElementById("byteCount"),
 
 onInput = () => {
-    let totalWords = 0, lineArr = textArea.value.split("\n").filter(el => el != "");
+    let totalWords = 0,
+        totalBytes = 0,
+        lineArr = textArea.value.split("\n").filter(el => el != "");
 
-    lineArr.forEach(line => totalWords += line.split(" ").filter(el => el != "").length);
+    lineArr.forEach(line => {
+        totalWords += line.split(" ").filter(el => el != "").length;
+        totalBytes += line.split(" ").filter(el => el != "").forEach(letter => totalBytes += letter.length);
+    });
 
     lcSpan.innerHTML = `${lineArr.length} line${lineArr.length != 1 ? "s" : ""}`;
     wcSpan.innerHTML = `${totalWords} word${totalWords != 1 ? "s" : ""}`;
+    bSpan.innerHTML = `${totalBytes} bytes${totalBytes != 1 ? "s" : ""}`;
 };
 
 textArea.oninput = () => onInput();
