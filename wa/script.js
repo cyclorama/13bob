e9cf7d077d4e14d8b841a74f03a0f6a1bd5d803e
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-03-01 00:30:21 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -6,9 +6,9 @@ let textArea = document.getElementById("text"),
 onInput = () => {
     let totalLines = 0, totalWords = 0, totalBytes = 0;
 
-    totalLines = textArea.value.split("\n").length - 1;
-    totalWords = textArea.value.split(" ").length - 1;
-    totalBytes = textArea.value.split("").length - 1;
+    totalLines = textArea.value.split("\n").length;
+    totalWords = textArea.value.split(" ").length;
+    totalBytes = textArea.value.split("").length;
 
     lcSpan.innerHTML = `${totalLines} line${totalLines != 1 ? "s" : ""}`;
     wcSpan.innerHTML = `${totalWords} word${totalWords != 1 ? "s" : ""}`;
