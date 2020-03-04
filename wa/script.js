8492be2f20f2ffc25ba1c8ec9f66bed01ab7ba32
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-03-04 19:51:40 +0000

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -7,7 +7,7 @@ onInput = () => {
     let textAreaVal = textArea.value, totalLines = 0, totalWords = 0, totalBytes = 0;
 
     totalLines = textAreaVal.split("\n").length;
-    totalWords = textAreaVal.split(" ").length > 0 ? textAreaVal.replace("\n", " ").split(" ").Length : 0;
+    totalWords = textAreaVal.replace("\n", " ").split(" ").Length;
     totalBytes = textAreaVal.split("").length;
 
     lcSpan.innerHTML = `${totalLines} line${totalLines != 1 ? "s" : ""}`;
