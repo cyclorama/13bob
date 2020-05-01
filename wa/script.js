9efeb6014219542eefa15049df01e7974add44c0
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-05-01 23:49:10 +0100

    Update script.js

--- wa/script.js
+++ wa/script.js
@@ -8,6 +8,5 @@ textArea.on("input", () => {
         totalUpper  = textAreaVal.split("").filter(p => p >= "A" && p <= "z" && p == p.toUpperCase()).length,
         totalLower  = textAreaVal.split("").filter(p => p >= "A" && p <= "z" && p == p.toLowerCase()).length;
 
-
     statArea.val(`line${totalLines != 1 ? "s" : ""}: ${totalLines}\nword${totalWords != 1 ? "s" : ""}: ${totalWords}\nbyte${totalBytes != 1 ? "s" : ""}: ${totalBytes}\nCHAR${totalUpper != 1 ? "S" : ""}: ${totalUpper}\nchar${totalLower != 1 ? "s" : ""}: ${totalLower}`);
 });
