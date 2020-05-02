b052140d6982d54cdc12aa97cc286e7cc7a4cac5
cyclorama <19229386+cyclorama@users.noreply.github.com>
2020-05-02 17:56:16 +0100

    Update title.js

--- title.js
+++ title.js
@@ -1,11 +1,11 @@
 !async function (title, h1, colours, sleep) {
-    await sleep(1000).then(() => h1.style.animation = html = '');
+    await sleep(1000).then(() => h1.style.animation = htm = '');
 
     for (let pos = 0; pos < title.length + colours.length; ) {
         for (let col = 0; col < colours.length; col++)
-            if (title[pos - col] && pos - col < title.length) html = `<font color="${colours[col]}">${title[pos - col]}</font>${html}`;
+            if (title[pos - col] && pos - col < title.length) htm = `<font color="${colours[col]}">${title[pos - col]}</font>${htm}`;
             
-        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substr(0, pos - title.length - 2)}${html}${title.substr(pos)}</a>`, pos++).then(() => html = '');
+        await sleep(35).then(() => h1.innerHTML = `<a href="//13bob.net/">${title.substr(0, pos - title.length - 2)}${htm}${title.substr(pos)}</a>`, pos++).then(() => htm = '');
     }
     h1.style.animation = 'heartbeat 1s alternate infinite';
 
